2919a09d11232d186b2411907c160f0bbcdd399f
Jas Delgado <jas@jasdelgado.com>
2023-04-07 15:53:39 -0400

    add waitlist email confirmation

--- src/routes/v0/waitlist.ts
+++ src/routes/v0/waitlist.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 import express from 'express';
+import SendGrid from '@sendgrid/mail';
+
+SendGrid.setApiKey(process.env.SENDGRID_API_KEY || '');
+
 import { prisma } from '../../app';
 
 const router = express.Router();
@@ -15,6 +19,16 @@ router.post('/', async (req: Request, res: Response) => {
 			createdTime: new Date(),
 		},
 	});
+
+	const message = {
+		from: { email: 'notify@vividly.love', name: 'Vividly' },
+		to: email,
+		subject: 'Thanks for your interest in Vividly!',
+		html: `<p>We'll send you an email when we're ready to launch and when we're ready for beta testers.</p>`,
+		templateId: 'd-57c39d4a3ca04864bc2c58c48e80f000',
+	};
+
+	await SendGrid.send(message);
 
 	res.json(waitlistEntry);
 });
