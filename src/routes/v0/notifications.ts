e274114139d085c1226f0e254784f90479b6c8b6
Jas Delgado <jas@jasdelgado.com>
2023-07-07 19:01:31 -0400

    paginate notifications

--- src/routes/v0/notifications.ts
+++ src/routes/v0/notifications.ts
@@ -7,37 +7,92 @@ import { auth } from '../../middleware/auth';
 
 const router = express.Router();
 
+const NOTIFS_PAGE_LENGTH = 50;
+
 // @route GET /v0/notifications
 // @desc Get notifications
 // @access Private
 router.get('/', auth, async (req: Request, res: Response) => {
 	try {
 		const user = req.user as RequestUser;
+		const cursor = parseInt(req.query.cursor as string);
 
-		const notifications = await prisma.notification.findMany({
-			where: {
-				userId: user.id,
-			},
-			orderBy: {
-				createdTime: 'desc',
-			},
+		const blockedUsers = await prisma.blockedUser.findMany({
 			select: {
-				id: true,
-				createdTime: true,
-				isUnread: true,
-				body: true,
-				sender: {
-					select: {
-						id: true,
-						username: true,
-						avatarSrc: true,
-						bio: true,
-						name: true,
+				blockedUserId: true,
+			},
+			where: {
+				blockerId: user.id,
+			},
+		});
+
+		let notifications = [];
+
+		if (!cursor) {
+			notifications = await prisma.notification.findMany({
+				where: {
+					userId: user.id,
+					sender: {
+						id: {
+							notIn: blockedUsers.map(blockedUser => blockedUser.blockedUserId),
+						},
 					},
 				},
-			},
-			take: 50,
-		});
+				orderBy: {
+					createdTime: 'desc',
+				},
+				select: {
+					id: true,
+					createdTime: true,
+					isUnread: true,
+					body: true,
+					sender: {
+						select: {
+							id: true,
+							username: true,
+							avatarSrc: true,
+							bio: true,
+							name: true,
+						},
+					},
+				},
+				take: NOTIFS_PAGE_LENGTH + 1,
+			});
+		} else {
+			notifications = await prisma.notification.findMany({
+				cursor: {
+					id: cursor,
+				},
+				where: {
+					userId: user.id,
+					sender: {
+						id: {
+							notIn: blockedUsers.map(blockedUser => blockedUser.blockedUserId),
+						},
+					},
+				},
+				orderBy: {
+					createdTime: 'desc',
+				},
+				take: NOTIFS_PAGE_LENGTH + 1,
+				skip: 1,
+				select: {
+					id: true,
+					createdTime: true,
+					isUnread: true,
+					body: true,
+					sender: {
+						select: {
+							id: true,
+							username: true,
+							avatarSrc: true,
+							bio: true,
+							name: true,
+						},
+					},
+				},
+			});
+		}
 
 		const unreadCount = await prisma.notification.count({
 			where: {
@@ -46,11 +101,22 @@ router.get('/', auth, async (req: Request, res: Response) => {
 			},
 		});
 
-		const totalCount = notifications.length;
+		const len = notifications.length;
 
-		return res
-			.status(200)
-			.json({ success: true, notifications, unreadCount, totalCount });
+		let newCursor = null;
+		if (len > NOTIFS_PAGE_LENGTH) {
+			newCursor = notifications[len - 2].id;
+		}
+
+		return res.status(200).json({
+			success: true,
+			data: {
+				notifications: notifications,
+				unreadCount: unreadCount,
+				totalCount: len,
+			},
+			cursor: newCursor,
+		});
 	} catch (err) {
 		console.error(err);
 		return res.status(500).json({ success: false, error: err });
